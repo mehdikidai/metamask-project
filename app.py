370e4107ec3af3ff609764649209af45927cdf33
mehdikidai <mehdikidai@gmail.com>
2026-03-13 14:14:07 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import csv
+import time
+import os
+from dotenv import load_dotenv
+
+file_env = load_dotenv()
+
+if not load_dotenv():
+    print(".env file not found")
+    exit()
+
+password = os.getenv('PASSWORD','123')
+
+
+with open('data.csv', mode='r', newline='', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        print(row['first_name'] + " your password " + password) 
+        time.sleep(1)
+
+# file = open('data.csv', mode='r', newline='', encoding='utf-8')
+# reader = csv.DictReader(file)
+# for row in reader:
+#     print(row['first_name'] + " your password " + password) 
+#     time.sleep(1)
+    
+# file.close()
